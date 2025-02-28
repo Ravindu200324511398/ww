75e223846474ce26a23733e12f5cf585d23f1d2e
Ravindu200324511398 <Kodikararsdp@gmail.com>
2025-03-01 01:31:15 +0530

    Update DBConnection.java

--- CRS/src/edu/ijse/layerd/db/DBConnection.java
+++ CRS/src/edu/ijse/layerd/db/DBConnection.java
@@ -11,72 +11,7 @@ import java.sql.SQLException;
  *
  * @author ravindusaranga
  */
-/*public class DBConnection {
-     private static final String URL = "jdbc:mysql://localhost:3306/CRSPROGRAM";
-    private static final String USER = "root";
-    private static final String PASSWORD = "mysqlQ1W3e5r7";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-
-    public static Object getInstance() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-}*/
-/*public class DBConnection {
-    private static DBConnection dBConnection;
-    private Connection connection;
-    
-    private DBConnection() throws ClassNotFoundException, SQLException{
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CRSPROGRAM", "root", "mysqlQ1W3e5r7");
-
-    }
-    
-    public static DBConnection getInstance() throws ClassNotFoundException, SQLException{
-        if(dBConnection == null){
-            dBConnection = new DBConnection();
-        }
-        return dBConnection;
-    }
-    
-    public Connection getConnection(){
-        return connection;
-    }
-}*/
-/*public class DBConnection {
-    private static DBConnection dbConnection;
-    private Connection connection;
-
-    private DBConnection() {
-        try {
-            // Load MySQL Driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CRSPROGRAM", "root", "mysqlQ1W3e5r7");
-        } catch (Exception e) {
-            throw new RuntimeException("Database Connection Error: " + e.getMessage());
-        }
-    }
-
-    public static DBConnection getInstance() {
-        if (dbConnection == null) {
-            dbConnection = new DBConnection();
-        }
-        return dbConnection;
-    }
-
-    public Connection getConnection() {
-        try {
-            if (connection == null || connection.isClosed()) {  // Ensure connection is open
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/CRSPROGRAM", "root", "mysqlQ1W3e5r7");
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Error getting DB connection: " + e.getMessage());
-        }
-        return connection;
-    }
-}*/
 public class DBConnection {
     private static DBConnection dbConnection;
     private Connection connection;
